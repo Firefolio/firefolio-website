2ddd757e185c1b8f03c039bdf1bacb13f1687fce
Unknown <b6010572@my.shu.ac.uk>
2018-06-20 13:58:17 +0000

    Toggled navbar now displays its children

--- js/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-	var navbar = document.querySelector("#nav");
-	var toggle = function () {
-		navbar.classList.toggle("toggled");
-	}
-
-	navbar.addEventListener("click", toggle);
-});
