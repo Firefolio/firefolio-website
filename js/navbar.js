3e613e33d55cf19ed25cafc726efc8b718dbf130
Unknown <b6010572@my.shu.ac.uk>
2018-06-20 13:38:01 +0000

    Made navbar responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,8 @@
-$(document).ready(function (argument) {
-	// body...
-})
+document.addEventListener("DOMContentLoaded", function () {
+	var navbar = document.querySelector("#nav");
+	var toggle = function () {
+		navbar.classList.toggle("toggled");
+	}
+
+	navbar.addEventListener("click", toggle);
+});
